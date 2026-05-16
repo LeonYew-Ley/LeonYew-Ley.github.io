fd6499928ddb7f8a357be645b6e3327138013e7f
LeonYew <learngenius@outlook.com>
2026-05-16 17:38:59 +0800

    fix: add CNAME for leonyew.space custom domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "blog.leonyew.space",
+    baseUrl: "leonyew.space",
     ignorePatterns: ["private", "templates", ".obsidian", "**/99_RES_private/**"],
     defaultDateType: "modified",
     theme: {
